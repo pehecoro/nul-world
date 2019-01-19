8bbe2084446b6d347d180dfae0c9ac583f8c5b79
pehecoro <pehecoro@hotmail.com>
2019-01-19 07:01:11 -0200

    Added Initial Project

---
+++ src/main/java/com/pehecoro/nulworld/NulWorldApplication.java
@@ -0,0 +1,14 @@
+package com.pehecoro.nulworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NulWorldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NulWorldApplication.class, args);
+    }
+
+}
+
